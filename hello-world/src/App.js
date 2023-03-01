7276fca496ee2576b327eace44beec5eb6ad73ab
gutooriba <tecnologiasmetta@gmail.com>
2023-02-28 23:04:28 -0300

    Link Inicial

---
+++ hello-world/src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <header className="App-header">
+      <h1>Sistema de Recados</h1>
+        <img src='logometta.jpg' className="App-logo" alt="logo" />
+         
+      
+       <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Entrar
+        </a>
+        <p>Ainda não possui uma conta?</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       Cadastrar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
